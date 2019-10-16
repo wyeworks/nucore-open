c743237934f94170c206d58feb3e1ad11708fb33
Jason Hanggi <jason@tablexi.com>
2019-10-16 21:43:56 +0200

    deployment

---
+++ config/deploy/ansible.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
+set :rails_env, "stage"
+
+server "localhost", user: "nucore", port: "2222", roles: %w(web app db)
+set :deploy_to, "/home/nucore/localhost"
