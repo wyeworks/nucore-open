ad81c7d39d7a4d7b95346a5bbc519e659fc2d257
Jason Hanggi <jason@tablexi.com>
2019-10-16 21:54:51 +0200

    ansible environment

--- config/deploy/ansible.rb
+++ config/deploy/ansible.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
-set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
-set :rails_env, "stage"
+set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "ansible"
+set :rails_env, "ansible"
 
 server "localhost", user: "nucore", port: "2222", roles: %w(web app db)
 set :deploy_to, "/home/nucore/localhost"
