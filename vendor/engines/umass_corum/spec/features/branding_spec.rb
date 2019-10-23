993392a0e4eb6c6347410346192e02f78b2bfaa2
Mak Arnautovic <me@mak.io>
2019-10-23 10:21:25 -0500

    Update branding spec

--- vendor/engines/umass_corum/spec/features/branding_spec.rb
+++ vendor/engines/umass_corum/spec/features/branding_spec.rb
@@ -7,6 +7,8 @@ RSpec.describe "Branding" do
 
     expect(page).to have_css("img[src*='.svg'][alt*='UMass']")
     expect(page).to have_content(/\d{4} University of Massachusetts/)
+    expect(page).to have_css("a[href='/']")
+    expect(page).to have_content(/CORUM/)
   end
 
 end
