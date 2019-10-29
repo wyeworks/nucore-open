03952c1d71739b49cff8389095716a0fe49664c9
Mak Arnautovic <me@mak.io>
2019-10-29 13:37:19 -0500

    Add eager load to staging env

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,6 +5,7 @@ require_relative "production.rb"
 Nucore::Application.configure do
 
   config.action_mailer.delivery_method = :sendmail # :smtp
+  config.eager_load = false
   Rails.application.routes.default_url_options =
     config.action_mailer.default_url_options = { host: "corum-test.umass.edu", protocol: "http" }
 
