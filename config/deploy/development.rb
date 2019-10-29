9544c479cc78715af4d911e43845b35e88cda881
Mak Arnautovic <me@mak.io>
2019-10-29 14:00:52 -0500

    Update 'stage' env names to 'staging'

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,7 +2,7 @@
 
 # TODO: change to master
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "ansible"
-set :rails_env, "stage"
+set :rails_env, "staging"
 
 server "ials-core-web-dev-01.it.umass.edu", user: "nucore", roles: %w(web app db)
 set :deploy_to, "/home/nucore/corum-dev.umass.edu"
