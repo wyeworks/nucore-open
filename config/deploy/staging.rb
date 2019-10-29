8639ae7eba3a4d5a50ec9ca3c4de437f34244ed0
Jason Hanggi <jason@tablexi.com>
2019-10-29 14:09:33 +0100

    fix "staging" variable in deploy

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
-set :rails_env, "stage"
+set :rails_env, "staging"
 
 server "ials-core-web-test-01.it.umass.edu", user: "nucore", roles: %w(web app db)
 set :deploy_to, "/home/nucore/corum-test.umass.edu"
