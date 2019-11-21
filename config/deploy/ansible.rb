d8d168a54467cd78479670fa77d8cb2ac64122a1
Jason Hanggi <jason@tablexi.com>
2019-11-21 12:11:38 +0100

    Changes to allow for full deploy to VirtualBox

--- config/deploy/ansible.rb
+++ config/deploy/ansible.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "ansible"
+set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
 set :rails_env, "staging"
 
 server "localhost", user: "nucore", port: "2222", roles: %w(web app db)
