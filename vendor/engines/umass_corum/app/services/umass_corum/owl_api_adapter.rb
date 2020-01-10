31ea891d0d8dbbfdb48585966099cdfdf212d4fe
Jason Hanggi <jason@tablexi.com>
2020-01-10 15:05:06 -0600

    Hook up adapter for UMass OWL research safety API (#35)
    # Release Notes
    
    Hook up adapter for UMass OWL research safety API

---
+++ vendor/engines/umass_corum/app/services/umass_corum/owl_api_adapter.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+module UmassCorum
+
+  class OwlApiAdapter
+
+    TIMEOUT = 3
+
+    def self.params
+      {
+        Server: Rails.env.production? ? "owl-ehs" : "owl-ehsstaging",
+        UserType: "Student",
+        fxn: "trcompleted",
+        Mode: "IALSCorum", # If you need to test in development and avoid IP violations, use "IALSDummy"
+      }
+    end
+
+    # Note: If you are not on the UMass VPN, both servers are inaccessible due to firewalling
+    def self.host
+       Rails.env.production? ? "owl.umass.edu" : "owlstage.umass.edu"
+    end
+
+    def self.fetch(netid)
+      ActiveSupport::Notifications.instrument "get_owl_certifications.umass_corum" do |payload|
+        payload[:emplid] = netid
+
+        uri = URI.parse("https://#{host}/owlj/servlet/OwlPreLogin")
+        uri.query = URI.encode_www_form(params.merge("ID" => netid))
+
+        Net::HTTP.start(uri.host, uri.port, open_timeout: TIMEOUT, read_timeout: TIMEOUT, use_ssl: true) do |http|
+          req = Net::HTTP::Get.new(uri)
+          response = http.request(req)
+          payload[:response] = response
+          response.body
+        end
+      end
+    end
+
+    def initialize(user)
+      @user = user
+    end
+
+    def certified?(certificate)
+      completed_certificate_ids.include?(certificate.name)
+    end
+
+    private
+
+    def completed_certificate_ids
+      response["certifications"].map { |certification_data| certification_data["id"] }
+    end
+
+    def response
+      return @response if @response
+
+      @response = JSON.parse(self.class.fetch(@user.username))
+      raise @response["error"].fetch("message") if @response["error"]
+
+      @response
+    end
+
+  end
+
+end
