1585c820ddb3cb2b360784495748ceba7f51a6f9
Jason Hanggi <jason@tablexi.com>
2020-07-09 17:45:30 -0500

    [#170385650] UMass Statement Tempate (#48)
    # Release Notes
    
    Initial formatting to match UMass's template for statements/invoices.
    
    # Screenshot
    
    Here is theirs:
    
    ![Screen Shot 2020-01-22 at 8 02 32 PM](https://user-images.githubusercontent.com/1099111/72950907-49068c80-3d52-11ea-8344-0815e3d40807.png)
    
    Here is what it looks like after the changes in this PR:
    <img width="810" alt="Screen Shot 2020-07-08 at 9 26 20 AM" src="https://user-images.githubusercontent.com/30355046/86931003-3a900f80-c0fd-11ea-81f4-e9db163d3653.png">
    
    
    # Additional Context
    
    I've never loved making these PDFs this way, but this is how we do it for the other schools. The code is kind of nasty because it's a lot of tables and styling. I took some liberties with not matching exact stylings from the example.
    
    There are a couple todos still in code while I'm waiting to hear back from Charlene.
    
    I also include the order number and date as columns which they don't have. I _think_, but I'm not sure they for Flow, they roll up everything into a line item or two in the invoice and then attach some logs that come from the machines. We will be changing their workflow a little bit.
    
    There's a good chance this will be one of those things where we merge it in as is and make iterative tweaks as they get more familiar with the system.

---
+++ vendor/engines/umass_corum/app/services/umass_corum/statement_pdf.rb
@@ -0,0 +1,239 @@
+# frozen_string_literal: true
+
+# 1 inch = 72 points
+# In general, we should use points when dealing with font-relational measurements,
+# but we can use inches when dealing with graphics and page layout.
+require "prawn/measurement_extensions"
+
+module UmassCorum
+
+  class StatementPdf < ::StatementPdf
+
+    include TextHelpers::Translation
+    def translation_scope
+      "umass_corum.statement_pdf"
+    end
+
+    attr_reader :pdf, :statement
+    delegate :facility, to: :statement
+
+    def options
+      super.merge(
+        left_margin: 0.25.in,
+        right_margin: 0.25.in,
+        bottom_margin: 0.5.in,
+        top_margin: 0.5.in,
+      )
+    end
+
+    CELL_STYLE = { border_width: 1, padding: 4 }.freeze
+    MAIN_FONT_SIZE = 9 # Points
+
+    def generate(pdf)
+      @pdf = pdf
+      pdf.default_leading = 2.5 # Points
+
+      draw_header
+      draw_bill_to_and_remit
+      draw_po_and_terms
+      draw_order_details_table
+      draw_footer
+    end
+
+    private
+
+    def draw_header
+      # Use inches when dealing with graphics
+      pdf.move_up(0.2.in)
+      pdf.image("#{UmassCorum::Engine.root}/app/assets/images/umass_corum/umass-ials-logo.jpg", height: 0.7.in)
+      pdf.move_up(0.2.in)
+      pdf.font_size(20) { pdf.text(text("invoice_header"), align: :right, style: :bold) }
+
+      pdf.move_down(MAIN_FONT_SIZE)
+      same_row(
+        method(:draw_address),
+        method(:draw_date_and_invoice_number)
+      )
+      pdf.move_down(8)
+    end
+
+    def draw_address
+      pdf.font_size(11) do
+        pdf.text text("header_address", facility: facility.name, address: facility.address), leading: 1.5
+      end
+    end
+
+    def draw_date_and_invoice_number
+      data = [
+        [text("date_header"), Statement.human_attribute_name(:id)],
+        [format_usa_date(@statement.created_at), @statement.invoice_number],
+      ]
+      pdf.font_size(MAIN_FONT_SIZE)
+      pdf.table(
+        data,
+        header: true,
+        cell_style: CELL_STYLE.merge(align: :center),
+        position: :right,
+        width: grid(4),
+      )
+    end
+
+    def draw_bill_to_and_remit
+      same_row(
+        method(:draw_bill_to_box),
+        method(:draw_remit_box),
+      )
+      pdf.move_down(16)
+    end
+
+    def draw_bill_to_box
+      data = [
+        [text("bill_to_header")],
+        [normalize_whitespace(@account.remittance_information) || "\n\n\n\n"],
+      ]
+      pdf.table(data,
+        cell_style: CELL_STYLE,
+        width: grid(5),
+      )
+    end
+
+    def draw_remit_box
+      data = [
+        [text("remit_box", address: @statement.facility.address)],
+      ]
+
+      pdf.table(data,
+        cell_style: CELL_STYLE.merge(align: :center),
+        position: :right,
+        width: grid(5),
+      )
+    end
+
+    def draw_po_and_terms
+      data = [
+        [text("po_number_header"), text("terms_header")],
+        ["\n", "\n"],
+      ]
+      pdf.table(
+        data,
+        header: true,
+        cell_style: CELL_STYLE.merge(align: :center),
+        position: :right,
+        width: grid(4),
+      )
+    end
+
+    def draw_order_details_table
+      data = [order_detail_headers] +
+        order_detail_rows +
+        [[{ content: pay_by_text, colspan: 5 }, { content: text("total", total: number_to_currency(total_due)), colspan: 2 }]]
+
+      description_width = grid(4)
+      other_width = grid((12 - 4).to_f / 6)
+      pdf.table(
+        data,
+        width: pdf.bounds.width,
+        cell_style: CELL_STYLE.merge(borders: [:left, :right]),
+        header: true,
+        column_widths: [other_width, other_width, other_width, description_width, other_width, other_width, other_width],
+      ) do
+        column([0, 5, 6]).style(align: :right)
+        row(0).style(borders: [:top, :bottom, :left, :right], align: :center)
+        row(-2).style(borders: [:bottom, :left, :right])
+        row(-1).style(borders: [:bottom, :left, :right])
+        row(-1).column(0).style(align: :center, font_style: :bold)
+        row(-1).column(-2).style(size: 14, font_style: :bold)
+      end
+    end
+
+    def draw_footer
+      # Start a new page if the the second row would get orphaned on its own page
+      pdf.start_new_page if pdf.cursor < 36
+      data = [
+        [
+          text("footer_headers.phone"),
+          text("footer_headers.fax"),
+          text("footer_headers.email"),
+          text("footer_headers.website"),
+        ],
+        [facility.phone_number, facility.fax_number, facility.email, text("contact_email")]
+      ]
+      pdf.table(
+        data,
+        width: grid(10),
+        position: :center,
+        cell_style: CELL_STYLE.merge(align: :center),
+      )
+    end
+
+    def order_detail_headers
+      [
+        text("order_detail_headers.quantity"),
+        text("order_detail_headers.date"),
+        text("order_detail_headers.order_number"),
+        text("order_detail_headers.description"),
+        text("order_detail_headers.unit_of_measure"),
+        text("order_detail_headers.rate"),
+        text("order_detail_headers.amount"),
+      ]
+    end
+
+    def order_detail_rows
+      order_details.map do |order_detail|
+        [
+          order_detail_quantity(order_detail),
+          format_usa_date(order_detail.ordered_at),
+          order_detail.to_s,
+          [order_detail.product, normalize_whitespace(order_detail.note)].map(&:presence).compact.join("\n"),
+          order_detail.product.quantity_as_time? ? "hr" : "",
+          number_to_currency(order_detail.actual_cost / order_detail.quantity),
+          number_to_currency(order_detail.actual_total),
+        ]
+      end
+    end
+
+    def pay_by_text
+      text("pay_by_card", url: facility.payment_url) if facility.payment_url.present?
+    end
+
+    def order_details
+      @statement
+        .order_details
+        .includes(:product, order: :user)
+        .order(fulfilled_at: :desc)
+    end
+
+    def total_due
+      order_details.map(&:actual_total).sum
+    end
+
+    # Run a series of procs that will all draw to the same row. At the end, move
+    # the cursor to the bottom of the tallest one.
+    def same_row(*blocks)
+      top = pdf.cursor
+      bottoms = Array(blocks).map do |block|
+        block.call
+        bottom = pdf.cursor
+        pdf.move_cursor_to(top)
+        bottom
+      end
+
+      pdf.move_cursor_to(bottoms.min)
+    end
+
+    # Use a 12-column grid. To get half the page, you can use grid(6). For a quarter, grid(3).
+    def grid(columns)
+      pdf.bounds.width * columns / 12
+    end
+
+    def order_detail_quantity(order_detail)
+      if order_detail.time_data.present? && order_detail.time_data.respond_to?(:billable_minutes)
+        minutes = order_detail.time_data.billable_minutes
+        QuantityPresenter.new(order_detail.product, minutes).to_s if minutes
+      else
+        QuantityPresenter.new(order_detail.product, order_detail.quantity).to_s
+      end
+    end
+  end
+
+end
