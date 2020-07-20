14f86dfa2ec10750b355c6cbf3df3a62c0a4704e
Gilad Shanan <30355046+giladshanan@users.noreply.github.com>
2020-07-20 15:18:06 -0500

    [#153972] Remove cancer center PriceGroup (#62)
    # Release Notes
    
    Building on https://github.com/tablexi/nucore-open/pull/2347, this removes the cancer center PriceGroup from settings.yml and seeds.rb.  
    
    Run `bundle exec rake cleanup:price_groups:cancer_center` to remove an existing cancer center PriceGroup.  Only works if there are not associated order_details.

--- vendor/engines/umass_corum/db/seeds.rb
+++ vendor/engines/umass_corum/db/seeds.rb
@@ -1,9 +1,3 @@
-cancer_center = PriceGroup.find_by(name: Settings.price_group.name.cancer_center)
-if cancer_center
-  cancer_center.price_policies.delete_all
-  cancer_center.delete
-end
-
 PriceGroup.create_with(is_internal: true, admin_editable: false, display_order: 4)
   .find_or_initialize_by(name: "Other academic (UMass Affiliated)").save!(validate: false)
 PriceGroup.create_with(is_internal: false, admin_editable: false, display_order: 3)
