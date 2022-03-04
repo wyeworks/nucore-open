1a1f3870e8f1773346ba6903ffbc06fd8b136f40
Joseph Simmons <josephdsimmons@gmail.com>
2022-03-04 10:26:29 -0600

    Makes umass_emplid uniqueness case sensitive, adjusts formatting

--- vendor/engines/umass_corum/app/models/umass_corum/user_extension.rb
+++ vendor/engines/umass_corum/app/models/umass_corum/user_extension.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 module UmassCorum
+
   module UserExtension
+
 	extend ActiveSupport::Concern
 
 	included do
-	  validates :umass_emplid, uniqueness: { allow_blank: true, case_sensitive: false }
+	  validates :umass_emplid, uniqueness: { allow_blank: true, case_sensitive: true }
 	end
+
   end
+
 end
--- vendor/engines/umass_corum/spec/models/user_spec.rb
+++ vendor/engines/umass_corum/spec/models/user_spec.rb
@@ -3,6 +3,6 @@ require "rails_helper"
 RSpec.describe User do
 	it "does requires unique employee ids" do
 		create(:user, umass_emplid: 'id_2')
-		should validate_uniqueness_of(:umass_emplid).case_insensitive
+		should validate_uniqueness_of(:umass_emplid)
 	end
 end
