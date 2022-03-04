90b37f44f34de2a7e09d7a15206161870f74631b
Joseph Simmons <josephdsimmons@gmail.com>
2022-03-04 10:32:46 -0600

    Removes tabs

--- vendor/engines/umass_corum/spec/models/user_spec.rb
+++ vendor/engines/umass_corum/spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User do
-	it "does requires unique employee ids" do
-		create(:user, umass_emplid: 'id_2')
-		should validate_uniqueness_of(:umass_emplid)
-	end
+  it "does requires unique employee ids" do
+	create(:user, umass_emplid: 'id_2')
+	should validate_uniqueness_of(:umass_emplid)
+  end
 end
