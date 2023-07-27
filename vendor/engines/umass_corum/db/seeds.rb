75d8b1b3ba2e31c93737c89a92f368306bb4baec
Joseph Simmons <josephdsimmons@gmail.com>
2023-07-27 09:43:26 -0500

    Updates PriceGroup creation in vendor seeds.rb

--- vendor/engines/umass_corum/db/seeds.rb
+++ vendor/engines/umass_corum/db/seeds.rb
@@ -1,7 +1,16 @@
-PriceGroup.create_with(is_internal: true, admin_editable: false, display_order: 4)
-  .find_or_initialize_by(name: "Other academic (UMass Affiliated)").save!(validate: false)
-PriceGroup.create_with(is_internal: false, admin_editable: false, display_order: 3)
-  .find_or_initialize_by(name: "Other academic (Non-UMass Affiliated)").save!(validate: false)
+PriceGroup.setup_global(
+  name: "Other academic (UMass Affiliated)",
+  is_internal: true,
+  admin_editable: false,
+  display_order: 4
+)
+
+PriceGroup.setup_global(
+  name: "Other academic (UMass Affiliated)",
+  is_internal: false,
+  admin_editable: false,
+  display_order: 3
+)
 
 FactoryBot.create(:api_speed_type, speed_type: "123456")
 FactoryBot.create(:api_speed_type, speed_type: "654321")
