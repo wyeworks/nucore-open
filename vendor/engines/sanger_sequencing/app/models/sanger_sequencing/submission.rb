bcac736447c72c89869ed5d0abd4d749d1785028
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-01-17 17:14:33 -0300

    Treate nil as default group

--- vendor/engines/sanger_sequencing/app/models/sanger_sequencing/submission.rb
+++ vendor/engines/sanger_sequencing/app/models/sanger_sequencing/submission.rb
@@ -32,15 +32,21 @@ module SangerSequencing
     }
 
     def self.for_product_group(product_group)
-      if product_group.present?
-        where(order_details: { product_id: SangerProduct.where(group: product_group).pluck(:product_id) })
-      else
+      if product_group.nil? || product_group == SangerProduct::DEFAULT_GROUP
         # Absence of product_group is conceptually the same
         # as having default group
         where.not(
           order_details: {
             product_id: SangerProduct.where.not(
               group: SangerProduct::DEFAULT_GROUP
+            ).pluck(:product_id),
+          }
+        )
+      else
+        where(
+          order_details: {
+            product_id: SangerProduct.where(
+              group: product_group
             ).pluck(:product_id),
           }
         )
