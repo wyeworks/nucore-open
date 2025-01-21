fc6bc1392f1b899f91004753262a787c53251a64
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-01-21 09:54:10 -0300

    Add quick way of copying primer name

--- vendor/engines/sanger_sequencing/app/assets/javascripts/sanger_sequencing/submission.js
+++ vendor/engines/sanger_sequencing/app/assets/javascripts/sanger_sequencing/submission.js
@@ -3,12 +3,35 @@
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
  */
+
+const SubmissionForm = {
+  handleCopyPrimer(event) {
+    const row = $(event.target).parents("tr");
+
+    if (row.length == 0) { return; }
+    const primerName = row.find(".js--primerName").val()
+
+    row.nextAll(":visible").find(".js--primerName").val(primerName);
+  },
+  setupCopyPrimerCallback(element) {
+    $(element).on("click", this.handleCopyPrimer);
+  }
+};
+
+(function() {
+  $(document).ready(function() {
+    SubmissionForm.setupCopyPrimerCallback($(".js--copyPrimerNameDown"));
+  });
+}());
+
 $(() => $(document).on("fields_added.nested_form_fields", function(event) {
   const sangerSequencingForm = $("form.edit_sanger_sequencing_submission");
   if (!(sangerSequencingForm.length > 0)) { return; }
   const row = $(event.target);
   const customerSampleIdField = row.find(".js--customerSampleId");
   const prevSamplePrimerName = row.prevAll(":visible:first").find(".js--primerName").val();
+
+  SubmissionForm.setupCopyPrimerCallback(row);
 
   customerSampleIdField.prop("disabled", true).val("Loading...");
   $.post(sangerSequencingForm.data("create-sample-url")).done(function(sample) {
