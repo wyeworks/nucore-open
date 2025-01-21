4a39a60e89afb70ba9703b8b962182e75ee310ca
Joaquin <joaquincorrea333@gmail.com>
2025-01-21 18:13:42 -0300

    Fix primer name migration on oracle db

---
+++ vendor/engines/sanger_sequencing/db/migrate/20250120141340_add_sample_primer_name.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class AddSamplePrimerName < ActiveRecord::Migration[4.2]
+  def up
+    unless ActiveRecord::Base.connection.column_exists?(:sanger_sequencing_samples, :primer_name)
+      add_column :sanger_sequencing_samples, :primer_name, :string
+    end
+  end
+
+  def down
+  end
+end
