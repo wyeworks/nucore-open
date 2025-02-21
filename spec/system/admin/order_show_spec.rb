65a8d850be3f4bcc91c42efccbbbb1d5cb975b97
Leticia Errandonea <leticia@wyeworks.com>
2025-02-21 16:54:48 -0300

    Show Actual duration days as days

--- spec/system/admin/order_show_spec.rb
+++ spec/system/admin/order_show_spec.rb
@@ -4,7 +4,7 @@ require "rails_helper"
 
 RSpec.describe "Order show" do
   let(:facility) { create(:setup_facility) }
-  let(:account) { create(:setup_account) }
+  let(:account) { create(:setup_account, facility:) }
   let!(:instrument) { create(:setup_instrument, :always_available, :daily_booking, facility:) }
   let(:reservation_order) { create(:setup_order, product: instrument, account:) }
   let!(:reservation) do
@@ -12,13 +12,17 @@ RSpec.describe "Order show" do
       :purchased_reservation,
       product: instrument,
       order_detail: reservation_order.order_details.first,
-      reserve_start_at: 1.day.from_now,
-      reserve_end_at: 4.days.from_now
+      reserve_start_at: 4.days.ago,
+      reserve_end_at: 1.day.ago,
+      actual_start_at: 4.days.ago,
+      actual_end_at: 1.day.ago
     )
   end
   let(:facility_administrator) { create(:user, :facility_administrator, facility:) }
 
   before do
+    order_detail = reservation_order.order_details.first
+    order_detail.complete!
     login_as facility_administrator
     visit facility_order_path(facility, reservation_order)
   end
@@ -33,7 +37,7 @@ RSpec.describe "Order show" do
   describe "reservation actual" do
     it "is correctly displayed in days" do
       # Actual is the fourth column
-      expect(all("tbody tr").first.all("td")[3]).to have_text("0", exact: true)
+      expect(all("tbody tr").first.all("td")[3]).to have_text("3", exact: true)
     end
   end
 
@@ -45,6 +49,7 @@ RSpec.describe "Order show" do
 
     it "shows the duration days field" do
       expect(page).to have_field("Duration Days", with: "3")
+      expect(page).to have_field("Actual duration days", with: "3")
     end
   end
 end
