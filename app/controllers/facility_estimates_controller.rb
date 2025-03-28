94a83419eebbede17e309b5e24b7e78d5fe6624c
Leticia Errandonea <leticia@wyeworks.com>
2025-03-28 18:14:24 -0300

    Create estimate

--- app/controllers/facility_estimates_controller.rb
+++ app/controllers/facility_estimates_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class FacilityEstimatesController < ApplicationController
+  include DateHelper
 
   admin_tab :all
   before_action { @active_tab = "admin_estimates" }
@@ -14,5 +15,22 @@ class FacilityEstimatesController < ApplicationController
 
   def new
     @estimate = current_facility.estimates.new
+  end
+
+  def create
+    expires_at = parse_usa_date(facility_estimate_params[:expires_at])
+    @estimate = current_facility.estimates.new(facility_estimate_params.merge(created_by_id: current_user.id, expires_at:))
+
+    if @estimate.save
+      flash[:notice] = t(".success")
+    else
+      render action: :new
+    end
+  end
+
+  private
+
+  def facility_estimate_params
+    params.require(:estimate).permit([:name, :user_id, :note, :expires_at])
   end
 end
