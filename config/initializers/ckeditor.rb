75b6c014582706243ffe40a99e560a32c4bbbc55
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-03-31 16:05:14 -0300

    Add note on ckeditor version

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Ckeditor.setup do |config|
+  # Ckeditor switched to a comercial license in v4.25
+  # so we use a previous version
+  #
   # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
   config.cdn_url = "//cdn.ckeditor.com/4.19.1/standard/ckeditor.js"
 end
