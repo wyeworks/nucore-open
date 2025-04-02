d3540e8ccf3d4b8db3fb3c0d68e9b039bb6f4cb0
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-04-02 09:29:41 -0300

    Fix bulk deposit number

--- app/assets/javascripts/bulk_note.js
+++ app/assets/javascripts/bulk_note.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
   var bulkNoteCheckbox = $('input[name="bulk_note_checkbox"]');
-  var bulkNoteInput = $("#bulk-note-input");
+  var bulkNoteInput = $(".js--bulkNoteInput");
   var rowNoteInputs = $(".row-note-input input")
 
   bulkNoteInput.hide();
