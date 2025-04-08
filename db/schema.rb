4468334460606e199cff7fdc95ac5eb11766dc21
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-04-08 11:42:40 -0300

    Fix schema version

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2025_04_01_180024) do
-ActiveRecord::Schema[7.0].define(version: 2025_03_26_164916) do
   create_table "account_facility_joins", id: :integer, charset: "utf8mb3", force: :cascade do |t|
     t.integer "facility_id", null: false
     t.integer "account_id", null: false
