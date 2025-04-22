5a4629f60fc7a891ac1f5514fa33d796cfc2441c
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-04-22 11:51:56 -0300

    Add message to log_events

--- db/migrate/20250421145232_add_log_event_eventy_type_index.rb
+++
@@ -1,5 +0,0 @@
-class AddLogEventEventyTypeIndex < ActiveRecord::Migration[7.0]
-  def change
-    add_index :log_events, :event_type
-  end
-end
---
+++ db/migrate/20250421145232_change_log_events.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class ChangeLogEvents < ActiveRecord::Migration[7.0]
+  def change
+    add_index :log_events, :event_type
+    add_column :log_events, :message, :string
+  end
+end
