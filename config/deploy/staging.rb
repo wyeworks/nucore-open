cdfec26a7815f0daa0304bce625c1670380b19db
Leticia Errandonea <leticia.errandonea@txidigital.com>
2025-05-16 18:27:54 -0300

    Remove all Deploy related code

--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-set :branch, ENV["CIRCLE_SHA1"] || ENV["REVISION"] || ENV["BRANCH_NAME"] || "master"
-set :rails_env, "staging"
-
-server "ials-core-web-test-01.it.umass.edu", user: "corum", roles: %w(web app db)
-set :deploy_to, "/home/corum/corum-test.umass.edu"
