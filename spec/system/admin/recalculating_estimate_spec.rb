cc91e900bff7e391fcebaa1a25f5682c52c95dad
Leticia Errandonea <leticia@wyeworks.com>
2025-05-19 14:11:46 -0300

    Show updated_at notice

--- spec/system/admin/recalculating_estimate_spec.rb
+++ spec/system/admin/recalculating_estimate_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe(
   let(:price_group) { user.price_groups.first }
   let!(:item) { create(:setup_item, facility:) }
   let!(:item_price_policy) { create(:item_price_policy, product: item, price_group:) }
-  let!(:estimate) { create(:estimate, user:, facility:, estimate_details: [create(:estimate_detail, product: item, quantity: 2)]) }
+  let!(:estimate) { create(:estimate, created_at: 3.hours.ago, updated_at: 3.hours.ago, user:, facility:, estimate_details: [create(:estimate_detail, product: item, quantity: 2, created_at: 3.hours.ago, updated_at: 3.hours.ago)]) }
 
   before { login_as director }
 
@@ -33,5 +33,8 @@ RSpec.describe(
     expect(page).to have_content "Estimate successfully recalculated"
     expect(page).to have_content ActionController::Base.helpers.number_to_currency(new_price * 2) # 2 items
     expect(page).to_not have_content ActionController::Base.helpers.number_to_currency(previous_price * 2)
+    updated_at = estimate.reload.estimate_details.maximum(:updated_at)
+    expect(updated_at).to be > 3.hours.ago
+    expect(page).to have_content "Prices last updated: #{updated_at.strftime('%m/%d/%Y %H:%M')}"
   end
 end
