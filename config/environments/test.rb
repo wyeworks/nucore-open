1cab4445d434961976c87c9ef283547f294d2077
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-05-22 13:55:06 -0300

    Additional bullet exception

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -111,6 +111,7 @@ Rails.application.configure do
     Bullet.add_safelist type: :unused_eager_loading, class_name: "Service", association: :current_offline_reservations
     Bullet.add_safelist type: :n_plus_one_query, class_name: "SecureRoomPricePolicy", association: :price_group
     Bullet.add_safelist type: :n_plus_one_query, class_name: "SecureRoomPricePolicy", association: :duration_rates
+    Bullet.add_safelist type: :n_plus_one_query, class_name: "SecureRoom", association: :alert
   end
 
   # Settings specified here will take precedence over those in config/application.rb.
