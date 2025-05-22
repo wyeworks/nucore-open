033e88664a5810369bb67df53d3d33c205cd9487
Leticia Errandonea <leticia@wyeworks.com>
2025-05-22 16:23:37 -0300

    Be explicit about how to handle rollback

--- db/migrate/20250519201017_add_price_group_to_estimates.rb
+++ db/migrate/20250519201017_add_price_group_to_estimates.rb
@@ -9,10 +9,14 @@ class AddPriceGroupToEstimates < ActiveRecord::Migration[7.0]
   def change
     add_reference :estimates, :price_group
 
-    # Populate the Price group for each existing Estimate.
-    # Just using the first price group for the facility, as we don't have any estimates in prod yet, so we don't need to worry about the price group.
-    Estimate.find_each do |estimate|
-      estimate.update(price_group: estimate.facility.price_groups.first)
+    reversible do |dir|
+      dir.up do
+        # Populate the Price group for each existing Estimate.
+        # Just using the first price group for the facility, as we don't have any estimates in prod yet, so we don't need to worry about the price group.
+        Estimate.find_each do |estimate|
+          estimate.update(price_group: estimate.facility.price_groups.first)
+        end
+      end
     end
   end
 end
