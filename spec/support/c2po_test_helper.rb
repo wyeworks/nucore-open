cac1e079d51150966e5f54ebd899b5783aaa228d
Leticia Errandonea <leticia@wyeworks.com>
2025-07-17 16:12:44 -0300

    Make sure specs pass when FF is on

--- spec/support/c2po_test_helper.rb
+++ spec/support/c2po_test_helper.rb
@@ -5,12 +5,20 @@
 # TODO: ideally this would live in c2po project spec
 module C2poTestHelper
 
+  def skip_if_credit_card_cannot_be_created
+    skip("credit card cannot be created") unless credit_card_create_enabled?
+  end
+
   def skip_if_credit_card_unreconcilable
     skip("credit card reconciliation disabled") unless credit_card_reconcile_enabled?
   end
 
   def credit_card_reconcile_enabled?
     Account.config.reconcilable_account_types.include?("CreditCardAccount")
+  end
+
+  def credit_card_create_enabled?
+    Account.config.creation_enabled_types.include?("CreditCardAccount")
   end
 
 end
