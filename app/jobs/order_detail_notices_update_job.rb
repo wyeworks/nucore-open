0e6e8dd6d3e04b43163e1e3682deccdf5eeff842
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-08-04 17:10:33 -0300

    Add rake task to backfill order detail notices

--- app/jobs/order_detail_notices_update_job.rb
+++ app/jobs/order_detail_notices_update_job.rb
@@ -5,8 +5,8 @@ class OrderDetailNoticesUpdateJob < ApplicationJob
     order_detail.set_problem_and_notices
 
     order_detail.update_columns(
-      notice_keys: order_detail.notices,
-      problem_keys: order_detail.problems,
+      notice_keys: order_detail.notice_keys,
+      problem_keys: order_detail.problem_keys,
       problem: order_detail.problem,
     )
   end
