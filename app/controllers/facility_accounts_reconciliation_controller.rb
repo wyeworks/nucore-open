fcd992e254b07c3704b9675cf79bc595aa4d3c3d
Diego Basterrech <diegobasterrech@hotmail.com>
2025-08-14 21:34:08 -0300

    Add FF

--- app/controllers/facility_accounts_reconciliation_controller.rb
+++ app/controllers/facility_accounts_reconciliation_controller.rb
@@ -47,22 +47,24 @@ class FacilityAccountsReconciliationController < ApplicationController
 
       order_details_by_statement = reconciler.order_details.group_by(&:statement)
 
-      order_details_by_statement.each do |statement, order_details|
-        reconciled_notes = order_details
-                           .select { |od| od.order_status.name == "Reconciled" }
-                           .filter_map(&:reconciled_note)
-                           .uniq
+      if SettingsHelper.feature_on?(:billing_log_events)
+        order_details_by_statement.each do |statement, order_details|
+          reconciled_notes = order_details
+                             .select { |od| od.order_status.name == "Reconciled" }
+                             .filter_map(&:reconciled_note)
+                             .uniq
 
-        unrecoverable_notes = order_details
-                              .select { |od| od.order_status.name == "Unrecoverable" }
-                              .filter_map(&:unrecoverable_note)
-                              .uniq
+          unrecoverable_notes = order_details
+                                .select { |od| od.order_status.name == "Unrecoverable" }
+                                .filter_map(&:unrecoverable_note)
+                                .uniq
 
-        metadata = {}
-        metadata[:reconciled_notes] = reconciled_notes if reconciled_notes.any?
-        metadata[:unrecoverable_notes] = unrecoverable_notes if unrecoverable_notes.any?
+          metadata = {}
+          metadata[:reconciled_notes] = reconciled_notes if reconciled_notes.any?
+          metadata[:unrecoverable_notes] = unrecoverable_notes if unrecoverable_notes.any?
 
-        LogEvent.log(statement, :closed, current_user, metadata: metadata)
+          LogEvent.log(statement, :closed, current_user, metadata: metadata)
+        end
       end
 
       flash[:notice] = "#{count} payment#{'s' unless count == 1} successfully updated" if count > 0
