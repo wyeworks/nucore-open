3a26cb37e43f0df71c686109f4ee2a7e3cc7605a
Diego Basterrech <diegobasterrech@hotmail.com>
2025-09-29 18:06:05 -0300

    update preview paths

--- config/application.rb
+++ config/application.rb
@@ -99,7 +99,7 @@ module Nucore
 
     # Override the default ("#{Rails.root}/**/spec/mailers/previews") to also load
     # previews from within our engines.
-    config.action_mailer.preview_path = "#{Rails.root}/**/spec/mailers/previews"
+    config.action_mailer.preview_paths = ["#{Rails.root}/**/spec/mailers/previews"]
     config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"
 
     # Prevent invalid (usually malicious) URLs from causing exceptions/issues
