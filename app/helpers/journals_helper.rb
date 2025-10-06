696ad3fa4fe7a5d97edf4f0a199f0c926d89debf
Leticia Errandonea <leticia@wyeworks.com>
2025-10-06 18:00:35 -0300

    Use I18n

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -2,6 +2,12 @@
 
 module JournalsHelper
   def journal_status_options(_journal)
-    [[' ', nil], ['Succeeded, no errors', 'succeeded'], ['Succeeded, with errors', 'succeeded_errors'], ['Failed', 'failed']]
+    [nil, 'succeeded', 'succeeded_errors', 'failed'].map do |status|
+      if status.present?
+        [t("facility_journals.status_options.#{status}"), status]
+      else
+        [t("facility_journals.status_options.blank"), nil]
+      end
+    end
   end
 end
