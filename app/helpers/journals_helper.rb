de7139f52db6a88e990645241d6599f883eb142e
Leticia Errandonea <leticia@wyeworks.com>
2025-10-06 14:19:37 -0300

    Add support for engines that could need the journal in the helper

--- app/helpers/journals_helper.rb
+++ app/helpers/journals_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module JournalsHelper
-  def journal_status_options
+  def journal_status_options(_journal)
     [[' ', nil], ['Succeeded, no errors', 'succeeded'], ['Succeeded, with errors', 'succeeded_errors'], ['Failed', 'failed']]
   end
 end
