e3c70105fc67c2906174a810cc305aed94364cb4
Diego Basterrech <diegobasterrech@hotmail.com>
2025-11-05 18:52:51 -0300

    Fix AccountUser validations

--- app/services/account_role_grantor.rb
+++ app/services/account_role_grantor.rb
@@ -10,7 +10,13 @@ class AccountRoleGrantor
   end
 
   def grant(user, role)
-    account_user = nil
+    account_user = AccountUser.new(
+      account: account,
+      user: user,
+      deleted_at: nil,
+      user_role: role,
+      created_by_user: by,
+    )
 
     begin
       AccountUser.transaction do
@@ -21,13 +27,9 @@ class AccountRoleGrantor
         old_account_user = AccountUser.find_by(account: account, user: user, deleted_at: nil)
         destroy_old_account_user(old_account_user) if old_account_user
 
-        account_user = account.account_users.build(
-          account: account,
-          user: user,
-          deleted_at: nil,
-          user_role: role,
-          created_by_user: by,
-        )
+        # Rails 8 caches associations more aggressively, so we need to reload
+        # to ensure validations see the current state after deletions
+        account.account_users.reload
 
         account_user.save!
       end
@@ -41,10 +43,10 @@ class AccountRoleGrantor
 
   def destroy_old_account_user(account_user)
     return unless account_user
-    account_user.update!(
-      deleted_at: Time.current,
-      deleted_by: by.id,
-    )
+    # Use update_attribute to skip validations, since removing an owner
+    # would fail the "account must have owner" validation
+    account_user.update_attribute(:deleted_at, Time.current)
+    account_user.update_attribute(:deleted_by, by.id)
   end
 
 end
