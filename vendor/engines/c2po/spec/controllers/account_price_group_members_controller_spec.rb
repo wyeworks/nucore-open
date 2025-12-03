335b5207161f10837aac20e358df27a377c397cb
Joaquin Correa <joaquin.correa@wyeworks.com>
2025-12-03 14:30:45 -0300

    Generalize account price group member spec

--- vendor/engines/c2po/spec/controllers/account_price_group_members_controller_spec.rb
+++ vendor/engines/c2po/spec/controllers/account_price_group_members_controller_spec.rb
@@ -11,11 +11,32 @@ RSpec.describe AccountPriceGroupMembersController do
 
     let(:partial_account_number) { build(:nufs_account).account_number[0..-4] }
     let(:price_group) { create(:price_group, facility: facility) }
-    let!(:global_account) { create(:nufs_account, :with_account_owner, account_number: "#{partial_account_number}234") }
-    let!(:facility_purchase_order) { create(:purchase_order_account, :with_account_owner, account_number: "#{partial_account_number}894", facility: facility) }
-    let!(:other_facility_purchase_order) { create(:purchase_order_account, :with_account_owner, account_number: "#{partial_account_number}542", facility: create(:facility)) }
-
-    let(:user) { create(:user, :facility_administrator, facility: facility) }
+    let(:global_account_type) { Account.config.global_account_types.first }
+    let(:facility_account_type) { Account.config.facility_account_types.first }
+    let!(:global_account) do
+      create(
+        global_account_type.underscore,
+        :with_account_owner,
+        account_number: "#{partial_account_number}234",
+      )
+    end
+    let!(:facility_purchase_order) do
+      create(
+        facility_account_type.underscore,
+        :with_account_owner,
+        account_number: "#{partial_account_number}894",
+        facility:,
+      )
+    end
+    let!(:other_facility_purchase_order) do
+      create(
+        facility_account_type.underscore,
+        :with_account_owner,
+        account_number: "#{partial_account_number}542",
+        facility: create(:facility),
+      )
+    end
+    let(:user) { create(:user, :facility_administrator, facility:) }
 
     it "limits the results to global and the facility" do
       sign_in user
