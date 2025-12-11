50da902fb080127c882409c87d110aca9c3531f7
Joaquin <20804419+joaquinco@users.noreply.github.com>
2025-12-11 15:56:09 -0300

    [UMASS-232] Globally managed facility accounts (#5857)
    * Allow facility accounts to be globally managed
    
    * Add facilities field to account new
    
    * Support facility_ids param in account builder
    
    * Add basic globally managed form specs

--- app/models/account_config.rb
+++ app/models/account_config.rb
@@ -23,6 +23,12 @@ class AccountConfig
     @facility_account_types ||= []
   end
 
+  # Account subclasses names that are facility accounts and can be
+  # managed globally by specifying which facilities they belong to.
+  def facility_account_globally_managed_types
+    @facility_account_globally_managed_types ||= []
+  end
+
   # Returns an array of subclassed Account object names that support statements.
   # Engines can append to this list.
   def statement_account_types
@@ -81,7 +87,7 @@ class AccountConfig
   # and the NullObject always returns `true` for cross_facility?.
   def account_types_for_facility(facility, action)
     types = account_types
-    types = types.select { |type| type.constantize.global? } if facility.try(:cross_facility?)
+    types = types.select { |type| globally_managed?(type) } if facility.try(:cross_facility?)
     types -= creation_disabled_types if action == :create
     types
   end
@@ -105,27 +111,37 @@ class AccountConfig
 
   # Returns true if this account type is limited to a single facility.
   def per_facility?(account_type)
-    facility_account_types.include?(account_type.to_s.classify)
+    facility_account_types.include?(to_account_class(account_type))
   end
 
   # Returns true if this account type can cross multiple facilities.
   def global?(account_type)
-    global_account_types.include?(account_type.to_s.classify)
+    global_account_types.include?(to_account_class(account_type))
+  end
+
+  def globally_managed?(account_type)
+    accounts = global_account_types + facility_account_globally_managed_types
+
+    accounts.include?(to_account_class(account_type))
   end
 
   # Returns true if this account type supports affiliate.
   def using_affiliate?(account_type)
-    affiliate_account_types.include?(account_type.to_s.classify)
+    affiliate_account_types.include?(to_account_class(account_type))
   end
 
   # Returns true if this account type supports statements.
   def using_statements?(account_type)
-    statement_account_types.include?(account_type.to_s.classify)
+    statement_account_types.include?(to_account_class(account_type))
   end
 
   # Returns true if this account type supports journal.
   def using_journal?(account_type)
-    journal_account_types.include?(account_type.to_s.classify)
+    journal_account_types.include?(to_account_class(account_type))
+  end
+
+  def to_account_class(account_type)
+    account_type.to_s.classify
   end
 
 end
---
+++ spec/requests/facility_accounts/create_spec.rb
@@ -0,0 +1,173 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "creating accounts" do
+  let(:admin) { create(:user, :administrator) }
+  let(:user) { create(:user) }
+
+  before do
+    login_as admin
+  end
+
+  describe "facility accounts" do
+    let(:facility_account_type) do
+      facility_account_class.to_s
+    end
+    let(:facility_account_class) { NufsAccount }
+
+    before do
+      allow(Account.config).to receive(:creation_enabled_types) do
+        [facility_account_type]
+      end
+      allow(Account.config).to receive(:facility_account_types) do
+        [facility_account_type]
+      end
+      allow(Account.config).to receive(:account_types) do
+        [facility_account_type]
+      end
+    end
+
+    context "when facility is cross facility" do
+      let(:facility) { Facility.cross_facility }
+
+      describe "per facility account" do
+        context "when it's globally managed" do
+          before do
+            allow(Account.config)
+              .to(receive(:facility_account_globally_managed_types)) do
+                [facility_account_type]
+              end
+          end
+
+          it "can create the account type" do
+            get new_facility_account_path(facility, owner_user_id: user.id)
+
+            expect(page).to(
+              have_content(facility_account_class.model_name.human)
+            )
+          end
+
+          it "includes a facility select field" do
+            get(
+              new_facility_account_path(
+                facility,
+                owner_user_id: user.id,
+                account_type: facility_account_type,
+              )
+            )
+
+            expect(page).to have_field(
+              "#{facility_account_type.underscore}[facility_ids][]",
+            )
+          end
+
+          describe "account creation" do
+            let(:some_facility) { create(:facility) }
+
+            it "can create the account assigning a facility" do
+              params = {
+                facility_account_type.underscore => {
+                  account_number: "213",
+                  description: "Some description",
+                  facility_ids: [some_facility.id],
+                }
+              }
+
+              expect do
+                post(
+                  facility_accounts_path(
+                    facility,
+                    owner_user_id: user.id,
+                    account_type: facility_account_type,
+                  ),
+                  params:,
+                )
+              end.to(
+                change(Account, :count).by(1)
+              )
+
+              expect(Account.last.facilities).to match([some_facility])
+            end
+          end
+        end
+
+        context "when it's not globally managed" do
+          before do
+            allow(Account.config)
+              .to(receive(:facility_account_globally_managed_types)) do
+                []
+              end
+          end
+
+          it "does not show the account type on creation" do
+            get new_facility_account_path(facility, owner_user_id: user.id)
+
+            expect(page).to have_content("Add Payment Source")
+            expect(page).not_to(
+              have_content(facility_account_class.model_name.human)
+            )
+          end
+        end
+      end
+    end
+
+    context "when facility is not cross facility" do
+      let(:facility) { create(:setup_facility) }
+
+      describe "per facility account" do
+        context "when the account is globally managed" do
+          before do
+            allow(Account.config)
+              .to(receive(:facility_account_globally_managed_types)) do
+                [facility_account_type]
+              end
+          end
+
+          it "shows the account type on creation" do
+            get new_facility_account_path(facility, owner_user_id: user.id)
+
+            expect(page).to(
+              have_content(facility_account_class.model_name.human)
+            )
+          end
+
+          it "does not show the facilities select field" do
+            get new_facility_account_path(
+              facility,
+              owner_user_id: user.id,
+              account_type: facility_account_type,
+            )
+
+            expect(page).to have_content("Add Payment Source")
+            expect(page).not_to have_field(
+              "#{facility_account_type.underscore}[facility_ids][]",
+            )
+          end
+
+          it "assigns current facility on creation" do
+            params = {
+              facility_account_type.underscore => {
+                account_number: "213",
+                description: "Some description",
+              }
+            }
+
+            expect do
+              post(
+                facility_accounts_path(
+                  facility,
+                  owner_user_id: user.id,
+                  account_type: facility_account_type,
+                ),
+                params:
+              )
+            end.to change(Account, :count).by(1)
+
+            expect(Account.last.facilities).to match([facility])
+          end
+        end
+      end
+    end
+  end
+end
