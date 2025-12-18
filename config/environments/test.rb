14b2b808ff9489654cf09321f4dcf53b66b0ca3b
Leticia Errandonea <leticia@wyeworks.com>
2025-12-18 17:08:06 -0300

    Skip Bullet warning

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -114,6 +114,8 @@ Rails.application.configure do
     Bullet.add_safelist type: :unused_eager_loading, class_name: "OrderDetail", association: :statement
     Bullet.add_safelist type: :unused_eager_loading, class_name: "Reservation", association: :order
     Bullet.add_safelist type: :unused_eager_loading, class_name: "Service", association: :current_offline_reservations
+    Bullet.add_safelist type: :unused_eager_loading, class_name: "Statement", association: :closed_events
+
     Bullet.add_safelist type: :n_plus_one_query, class_name: "SecureRoomPricePolicy", association: :price_group
     Bullet.add_safelist type: :n_plus_one_query, class_name: "SecureRoomPricePolicy", association: :duration_rates
     Bullet.add_safelist type: :n_plus_one_query, class_name: "SecureRoom", association: :alert
