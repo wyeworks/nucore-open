fa44b38c0091eeb6d44650a7b805cf448508507d
Diego Basterrech <diegobasterrech@hotmail.com>
2026-02-18 15:25:03 -0300

    Use CanCanCan instead of custom authorization for granular permissions

--- spec/services/default_facility_homepage_redirector_spec.rb
+++ spec/services/default_facility_homepage_redirector_spec.rb
@@ -24,5 +24,30 @@ RSpec.describe DefaultFacilityHomepageRedirector do
         expect(DefaultFacilityHomepageRedirector.redirect_path(facility, user)).to eq path
       end
     end
+
+    context "when user has only granular permissions", feature_setting: { granular_permissions: true } do
+      before do
+        create(:facility_user_permission, user: user, facility: facility, assign_permissions: true)
+      end
+
+      it "redirects to the staff page" do
+        path = "/#{I18n.t('facilities_downcase')}/#{facility.url_name}/facility_users"
+
+        expect(DefaultFacilityHomepageRedirector.redirect_path(facility, user)).to eq path
+      end
+    end
+
+    context "when user has a facility role and granular permissions", feature_setting: { granular_permissions: true } do
+      before do
+        UserRole.grant(user, UserRole::FACILITY_STAFF, facility)
+        create(:facility_user_permission, user: user, facility: facility, assign_permissions: true)
+      end
+
+      it "redirects to orders (not staff page)" do
+        path = "/#{I18n.t('facilities_downcase')}/#{facility.url_name}/orders"
+
+        expect(DefaultFacilityHomepageRedirector.redirect_path(facility, user)).to eq path
+      end
+    end
   end
 end
