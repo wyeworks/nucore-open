0be8d5c2450a514e8f7ef925a86b33607cb79762
Diego Basterrech <diegobasterrech@hotmail.com>
2026-02-20 17:42:34 -0300

    Show manage facility only for implemented permissions

--- app/services/default_facility_homepage_redirector.rb
+++ app/services/default_facility_homepage_redirector.rb
@@ -12,12 +12,14 @@ class DefaultFacilityHomepageRedirector
     end
   end
 
+  # Returns true if the user has no facility role but has an implemented granular permission.
+  # Update the `exists?` conditions as new granular permissions are implemented.
   def self.granular_permissions_only?(user, facility)
     return false unless SettingsHelper.feature_on?(:granular_permissions)
     return false if user.administrator?
 
     !UserRole.exists?(user: user, facility: facility) &&
-      user.facility_user_permissions.exists?(facility: facility)
+      user.facility_user_permissions.exists?(facility: facility, assign_permissions: true)
   end
 
 end
