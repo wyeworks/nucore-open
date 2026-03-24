cce0b6ab82da92bfffd6546b9ef8bb03801f4eae
Leticia Errandonea <leticia@wyeworks.com>
2026-03-24 10:46:36 -0300

    Fix Users tab not visible for Facility Admins

--- app/presenters/nav_tab/link_collection.rb
+++ app/presenters/nav_tab/link_collection.rb
@@ -123,7 +123,8 @@ class NavTab::LinkCollection
   end
 
   def admin_users
-    if single_facility? && (ability.can?(:manage, User) || ability.can?(:manage, FacilityUserPermission))
+    # TODO: This is reverted back to fix NU-473, but a better fix is needed so both Granular permissions and role-based permissions work.
+    if single_facility? && (ability.can?(:administer, User) || ability.can?(:manage, FacilityUserPermission))
       NavTab::Link.new(tab: :admin_users, url: facility_users_path(facility))
     end
   end
