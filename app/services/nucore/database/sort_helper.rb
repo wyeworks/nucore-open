ed7bd6f88ce3bac0faf9222bf0bd557ccdf0a205
Leticia Errandonea Mattos <leticia@wyeworks.com>
2026-04-14 13:16:27 -0300

    Tech task: Add support for Postgres (#6113)
    * Add support for Postgres DB
    
    * Simplify spec so it does not require access list

--- app/services/nucore/database/sort_helper.rb
+++ app/services/nucore/database/sort_helper.rb
@@ -13,19 +13,25 @@ module Nucore
       module ClassMethods
 
         # MySQL by default will sort with nulls coming before anything with a value.
-        # Oracle by defaults puts the nulls at the end in ASC mode
+        # Oracle and PostgreSQL by default put the nulls at the end in ASC mode
         def order_by_asc_nulls_first(field)
-          order_by = Nucore::Database.oracle? ? "#{field} ASC NULLS FIRST" : field.to_s
+          order_by = Nucore::Database.oracle? || Nucore::Database.postgresql? ? "#{field} ASC NULLS FIRST" : field.to_s
           order(Arel.sql(sanitize_sql_for_order(order_by)))
         end
 
         def order_by_desc_nulls_first(field)
-          order_by = Nucore::Database.oracle? ? "#{field} DESC NULLS FIRST" : "#{field} DESC"
+          order_by = Nucore::Database.oracle? || Nucore::Database.postgresql? ? "#{field} DESC NULLS FIRST" : "#{field} DESC"
           order(Arel.sql(sanitize_sql_for_order(order_by)))
         end
 
         def order_by_asc_nulls_last(field)
-          order_by = Nucore::Database.oracle? ? field.to_s : "#{field} IS NULL, #{field} ASC"
+          order_by = if Nucore::Database.oracle?
+                       field.to_s
+                     elsif Nucore::Database.postgresql?
+                       "#{field} ASC NULLS LAST"
+                     else
+                       "#{field} IS NULL, #{field} ASC"
+                     end
           order(Arel.sql(sanitize_sql_for_order(order_by)))
         end
 
