1d5810e726b0b949dd4d96bc682903ef37165e7c
Joaquin Correa <joaquin.correa@wyeworks.com>
2026-04-30 08:08:42 -0300

    Fix read access migration

--- db/migrate/20260422115005_add_read_access_to_facility_user_permissions.rb
+++ db/migrate/20260422115005_add_read_access_to_facility_user_permissions.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 class AddReadAccessToFacilityUserPermissions < ActiveRecord::Migration[8.0]
+  class FacilityUserPermission < ActiveRecord::Base
+  end
+
   def up
     add_column :facility_user_permissions, :read_access, :boolean, default: false, null: false
-    execute "UPDATE facility_user_permissions SET read_access = true"
+    FacilityUserPermission.update_all(read_access: true)
   end
 
   def down
