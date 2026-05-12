ffe164e3e0f4943875dab52c4d5c79dac62c4006
Joaquin Correa <joaquin.correa@wyeworks.com>
2026-05-12 10:46:25 -0300

    Undo ability_matchers.rb changes

--- spec/support/matchers/ability_matchers.rb
+++ spec/support/matchers/ability_matchers.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-RSpec::Matchers.define :be_allowed_to do |actions, object|
+RSpec::Matchers.define :be_allowed_to do |action, object|
   description do
-    "be allowed to #{actions} " +
+    "be allowed to #{action} " +
       if object.respond_to?(:model_name)
         object.model_name.human.pluralize
       elsif object.class.respond_to?(:model_name)
@@ -13,15 +13,13 @@ RSpec::Matchers.define :be_allowed_to do |actions, object|
   end
 
   match do |ability|
-    [actions].flatten.all? do |action|
-      ability.can?(action, object)
-    end
+    ability.can?(action, object)
   end
 end
 
 def it_is_allowed_to(actions, object = nil)
   Array(actions).each do |action|
-    it "is allowed to #{action} #{object}" do
+    it "is allowed to #{action}" do
       target = object || yield
       expect(subject).to be_allowed_to(action, target)
     end
