58fd904c3a465d2cbd7ec07038f963677750b372
Joaquin Correa <joaquin.correa@wyeworks.com>
2026-05-13 17:00:15 -0300

    Do not retry bulk import job

--- app/jobs/bulk_import_job.rb
+++ app/jobs/bulk_import_job.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 class BulkImportJob < ApplicationJob
+  # Do not retry this job
+  retry_on StandardError, attempts: 1
+
   def perform(bulk_import)
     bulk_import.load!
   end
