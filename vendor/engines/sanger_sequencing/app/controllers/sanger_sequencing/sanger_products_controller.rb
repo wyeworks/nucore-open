5dd3f89302c6b643857afd1b4932eb28795cea31
Diego Basterrech <diegobasterrech@hotmail.com>
2026-05-20 11:22:16 -0300

    Fix tests

--- vendor/engines/sanger_sequencing/app/controllers/sanger_sequencing/sanger_products_controller.rb
+++ vendor/engines/sanger_sequencing/app/controllers/sanger_sequencing/sanger_products_controller.rb
@@ -5,7 +5,7 @@ module SangerSequencing
   class SangerProductsController < BaseController
     before_action :set_resources
     before_action { @active_tab = "admin_products" }
-    before_action { authorize! :manage, @product }
+    before_action { authorize! :update, @product }
 
     layout "two_column"
     admin_tab :all
