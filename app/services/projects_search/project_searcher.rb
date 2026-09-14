48a1e6feee81bf51848d3187924ec82b27f780a8
Joaquin Correa <joaquin.correa@wyeworks.com>
2026-09-14 15:56:31 -0300

    Remove unneeded distinct

--- app/services/projects_search/project_searcher.rb
+++ app/services/projects_search/project_searcher.rb
@@ -11,7 +11,7 @@ module ProjectsSearch
     def options
       projects =
         if current_facility.cross_facility?
-          Project.where(id: order_details.select("distinct project_id")).order(:name)
+          Project.where(id: order_details.select("project_id")).order(:name)
         else
           project_ids =
             current_facility
