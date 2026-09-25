7404a7474ab52d9c2419b93132467d55be4222be
Leticia Errandonea <leticia@wyeworks.com>
2026-09-25 14:02:14 -0300

    Remove unused files

--- spec/mailers/previews/account_search_result_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AccountSearchResultMailerPreview < ActionMailer::Preview
-
-  def search_result
-    AccountSearchResultMailer.search_result("example@example.com", "123", Facility.first)
-  end
-
-end
